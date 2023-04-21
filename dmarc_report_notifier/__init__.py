78c34b60716107876814c0c94867d27e517dc950
Andrew Kvalheim <Andrew@Kvalhe.im>
2023-04-21 15:09:33 -0700

    Format code

--- dmarc_report_notifier/__init__.py
+++ dmarc_report_notifier/__init__.py
@@ -8,68 +8,80 @@ from parsedmarc.mail import IMAPConnection
 from smtplib import SMTP
 
 
-configureLogging(level=environ.get('LOG_LEVEL', 'INFO'))
+configureLogging(level=environ.get("LOG_LEVEL", "INFO"))
+
+
+def view(report):
+    title = report["report_metadata"]["org_name"]
+    json = as_json(report, ensure_ascii=False, indent=2)
+
+    return f"<h2>{as_html(title)}</h2><pre>{as_html(json)}</pre>"
 
 
 def main():
     config = {
-        'imap_host': environ['IMAP_HOST'],
-        'imap_username': environ['IMAP_USERNAME'],
-        'imap_password': environ['IMAP_PASSWORD'],
-        'imap_folder_processed': environ['IMAP_FOLDER_PROCESSED'],
-        'imap_folder_unprocessed': environ['IMAP_FOLDER_UNPROCESSED'],
-        'smtp_host': environ['SMTP_HOST'],
-        'smtp_port': int(environ['SMTP_PORT']),
-        'smtp_username': environ['SMTP_USERNAME'],
-        'smtp_password': environ['SMTP_PASSWORD'],
-        'sender': environ['SENDER'],
-        'recipient': environ['RECIPIENT'],
-    };
+        "imap_host": environ["IMAP_HOST"],
+        "imap_username": environ["IMAP_USERNAME"],
+        "imap_password": environ["IMAP_PASSWORD"],
+        "imap_folder_processed": environ["IMAP_FOLDER_PROCESSED"],
+        "imap_folder_unprocessed": environ["IMAP_FOLDER_UNPROCESSED"],
+        "smtp_host": environ["SMTP_HOST"],
+        "smtp_port": int(environ["SMTP_PORT"]),
+        "smtp_username": environ["SMTP_USERNAME"],
+        "smtp_password": environ["SMTP_PASSWORD"],
+        "sender": environ["SENDER"],
+        "recipient": environ["RECIPIENT"],
+    }
 
     reports = get_dmarc_reports_from_mailbox(
         connection=IMAPConnection(
-            host=config['imap_host'],
-            user=config['imap_username'],
-            password=config['imap_password']
+            host=config["imap_host"],
+            user=config["imap_username"],
+            password=config["imap_password"],
         ),
-        reports_folder=config['imap_folder_unprocessed'],
-        archive_folder=config['imap_folder_processed']
+        reports_folder=config["imap_folder_unprocessed"],
+        archive_folder=config["imap_folder_processed"],
     )
 
     notification_issues, notification_reports = set(), []
-    for report in reports['aggregate_reports']:
+    for report in reports["aggregate_reports"]:
         issues = set()
-        for record in report['records']:
-            if record['policy_evaluated']['dkim'] == 'fail': issues.add('DKIM failure')
-            elif not record['alignment']['dkim']: issues.add('DKIM misalignment')
-            if record['policy_evaluated']['spf'] == 'fail': issues.add('SPF failure')
-            elif not record['alignment']['spf']: issues.add('SPF misalignment')
-        info('Report %s from %s: %s',
-            report['report_metadata']['report_id'],
-            report['report_metadata']['org_name'],
-            ', '.join(issues) if issues else 'OK'
+        for record in report["records"]:
+            if record["policy_evaluated"]["dkim"] == "fail":
+                issues.add("DKIM failure")
+            elif not record["alignment"]["dkim"]:
+                issues.add("DKIM misalignment")
+            if record["policy_evaluated"]["spf"] == "fail":
+                issues.add("SPF failure")
+            elif not record["alignment"]["spf"]:
+                issues.add("SPF misalignment")
+        info(
+            "Report %s from %s: %s",
+            report["report_metadata"]["report_id"],
+            report["report_metadata"]["org_name"],
+            ", ".join(issues) if issues else "OK",
         )
         if issues:
             notification_issues.update(issues)
             notification_reports.append(report)
 
     if notification_issues:
-        info('Send email: from %s, to %s', config['sender'], config['recipient'])
+        info("Send email: from %s, to %s", config["sender"], config["recipient"])
 
-        html = ''.join([f"<h2>{as_html(report['report_metadata']['org_name'])}</h2><pre>{as_html(as_json(report, ensure_ascii=False, indent=2))}</pre>" for report in notification_reports])
+        html = "".join([view(report) for report in notification_reports])
 
         message = EmailMessage()
-        message['From'] = config['sender']
-        message['To'] = config['recipient']
-        message['Subject'] = ', '.join(notification_issues)
-        message.set_content(html, subtype='html')
+        message["From"] = config["sender"]
+        message["To"] = config["recipient"]
+        message["Subject"] = ", ".join(notification_issues)
+        message.set_content(html, subtype="html")
 
-        with SMTP(config['smtp_host'], config['smtp_port']) as smtp:
+        with SMTP(config["smtp_host"], config["smtp_port"]) as smtp:
             smtp.starttls()
-            smtp.login(config['smtp_username'], config['smtp_password'])
+            smtp.login(config["smtp_username"], config["smtp_password"])
             smtp.send_message(message)
             smtp.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
